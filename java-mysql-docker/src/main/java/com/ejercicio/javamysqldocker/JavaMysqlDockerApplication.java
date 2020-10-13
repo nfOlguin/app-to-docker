c449dee6737661bf94bd6473900f68aae3f863e8
naaman falcon <naamanalejandrofalcon@gmail.com>
2020-10-13 13:24:07 -0300

    agrego proyecto spring boot con conexión a mysql on docker

---
+++ java-mysql-docker/src/main/java/com/ejercicio/javamysqldocker/JavaMysqlDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio.javamysqldocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMysqlDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMysqlDockerApplication.class, args);
+		System.out.println("comienza...");
+	}
+}
