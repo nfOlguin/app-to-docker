496f430ffa34a7a04c54b8019591c7c1b51707d0
naaman falcon <naamanalejandrofalcon@gmail.com>
2020-10-13 14:44:13 -0300

    agrego parent entity y user entity

---
+++ java-mysql-docker/src/main/java/com/ejercicio/javamysqldocker/model/ParentEntity.java
@@ -0,0 +1,25 @@
+package com.ejercicio.javamysqldocker.model;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable {
+
+    private static final long serialVersionUID = 7639415200582386221L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="id", unique = true, nullable = false)
+    private Long id;
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ java-mysql-docker/src/main/java/com/ejercicio/javamysqldocker/model/User.java
@@ -0,0 +1,66 @@
+package com.ejercicio.javamysqldocker.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Users")
+@Access(AccessType.FIELD)
+public class User extends ParentEntity{
+
+    private static final long serialVersionUID = 7425453924182544028L;
+
+    @Column(name="first_name", nullable = false, length = 255)
+    private String firstName;
+
+    @Column(name="second_name", nullable = false, length = 255)
+    private String secondName;
+
+    @Column(name="first_surename", nullable = false, length = 255)
+    private String firstSurename;
+
+    @Column(name="phone", nullable = true, length = 30)
+    private String phone;
+
+    @Column(name="address", nullable = true, length = 255)
+    private String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getFirstSurename() {
+        return firstSurename;
+    }
+
+    public void setFirstSurename(String firstSurename) {
+        this.firstSurename = firstSurename;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
